d982765fea2e28f0b23cf864384344aa32341386
Jean-Philippe Braun <eon@patapon.info>
2012-01-10 12:22:20 +0100

    Port to GDBus bindings

--- src/dbus.js
+++ src/dbus.js
@@ -15,98 +15,65 @@
 **/
 
 const DBus = imports.dbus;
+const Gio = imports.gi.Gio;
 const Lang = imports.lang;
 
-const PropIFace = {
-    name: 'org.freedesktop.DBus.Properties',
-    signals: [{ name: 'PropertiesChanged',
-                inSignature: 'a{sv}'}]
-};
+const PropertiesIface = <interface name="org.freedesktop.DBus.Properties">
+<signal name="PropertiesChanged">
+    <arg type="s" direction="out" />
+    <arg type="a{sv}" direction="out" />
+    <arg type="as" direction="out" />
+</signal>
+</interface>;
+const PropertiesProxy = Gio.DBusProxy.makeProxyWrapper(PropertiesIface);
 
-const MediaServer2IFace = {
-    name: 'org.mpris.MediaPlayer2',
-    methods: [{ name: 'Raise',
-                inSignature: '',
-                outSignature: '' },
-              { name: 'Quit',
-                inSignature: '',
-                outSignature: '' }],
-    properties: [{ name: 'CanRaise',
-                   signature: 'b',
-                   access: 'read'},
-                 { name: 'CanQuit',
-                   signature: 'b',
-                   access: 'read'},
-                 { name: 'Identity',
-                   signature: 's',
-                   access: 'read'},
-                 { name: 'DesktopEntry',
-                   signature: 's',
-                   access: 'read'}],
-};
+const MediaServer2Iface = <interface name="org.mpris.MediaPlayer2">
+<method name="Raise" />
+<method name="Quit" />
+<property name="CanRaise" type="b" access="read" />
+<property name="CanQuit" type="b" access="read" />
+<property name="Identity" type="s" access="read" />
+<property name="DesktopEntry" type="s" access="read" />
+</interface>;
+const MediaServer2Proxy = Gio.DBusProxy.makeProxyWrapper(MediaServer2Iface);
 
-const MediaServer2PlayerIFace = {
-    name: 'org.mpris.MediaPlayer2.Player',
-    methods: [{ name: 'PlayPause',
-                inSignature: '',
-                outSignature: '' },
-              { name: 'Pause',
-                inSignature: '',
-                outSignature: '' },
-              { name: 'Play',
-                inSignature: '',
-                outSignature: '' },
-              { name: 'Stop',
-                inSignature: '',
-                outSignature: '' },
-              { name: 'Next',
-                inSignature: '',
-                outSignature: '' },
-              { name: 'Previous',
-                inSignature: '',
-                outSignature: '' },
-              { name: 'SetPosition',
-                inSignature: 'ox',
-                outSignature: '' }],
-    properties: [{ name: 'Metadata',
-                   signature: 'a{sv}',
-                   access: 'read'},
-                 { name: 'Shuffle',
-                   signature: 'b',
-                   access: 'readwrite'},
-                 { name: 'Rate',
-                   signature: 'd',
-                   access: 'readwrite'},
-                 { name: 'LoopStatus',
-                   signature: 'b',
-                   access: 'readwrite'},
-                 { name: 'Volume',
-                   signature: 'd',
-                   access: 'readwrite'},
-                 { name: 'PlaybackStatus',
-                   signature: 's',
-                   access: 'read'},
-                 { name: 'Position',
-                   signature: 'x',
-                   access: 'read'},
-                 { name: 'CanGoNext',
-                   signature: 'b',
-                   access: 'read'},
-                 { name: 'CanGoPrevious',
-                   signature: 'b',
-                   access: 'read'},
-                 { name: 'CanPlay',
-                   signature: 'b',
-                   access: 'read'},
-                 { name: 'CanPause',
-                   signature: 'b',
-                   access: 'read'},
-                 { name: 'CanSeek',
-                   signature: 'b',
-                   access: 'read'}],
-    signals: [{ name: 'Seeked',
-                inSignature: 'x' }]
-};
+const MediaServer2PlayerIface = <interface name="org.mpris.MediaPlayer2.Player">
+<method name="PlayPause" />
+<method name="Pause" />
+<method name="Play" />
+<method name="Stop" />
+<method name="Next" />
+<method name="Previous" />
+<method name="SetPosition">
+    <arg type="o" direction="in" />
+    <arg type="x" direction="in" />
+</method>
+<property name="Metadata" type="a{sv}" access="read" />
+<property name="Volume" type="d" access="readwrite" />
+<property name="PlaybackStatus" type="s" access="read" />
+<property name="Position" type="x" access="read" />
+<signal name="Seeked">
+    <arg type="x" direction="out" />
+</signal>
+</interface>
+const MediaServer2PlayerProxy = Gio.DBusProxy.makeProxyWrapper(MediaServer2PlayerIface);
+
+const MediaServer2PlaylistsIface = <interface name="org.mpris.MediaPlayer2.Playlists">
+<method name="ActivatePlaylist">
+    <arg type="o" direction="in" />
+</method>
+<method name="GetPlaylists">
+    <arg type="u" direction="in" />
+    <arg type="u" direction="in" />
+    <arg type="s" direction="in" />
+    <arg type="b" direction="in" />
+    <arg type="a{oss}" direction="out" />
+</method>
+<property name="PlaylistCount" type="u" access="read" />
+<property name="Orderings" type="as" access="read" />
+<property name="ActivePlaylist" type="(b(oss))" access="read" />
+</interface>
+const MediaServer2PlaylistsProxy = Gio.DBusProxy.makeProxyWrapper(MediaServer2PlaylistsIface);
 
 const MediaServer2PlaylistsIFace = {
     name: 'org.mpris.MediaPlayer2.Playlists',
@@ -130,161 +97,22 @@ const MediaServer2PlaylistsIFace = {
                    access: 'read' }]
 };
 
-function Prop() {
-    this._init.apply(this, arguments);
+function Properties(owner) {
+    return new PropertiesProxy(Gio.DBus.session, owner,
+                               '/org/mpris/MediaPlayer2');
 }
 
-Prop.prototype = {
-    _init: function(owner) {
-        DBus.session.proxifyObject(this, owner, '/org/mpris/MediaPlayer2', this);
-    }
-}
-DBus.proxifyPrototype(Prop.prototype, PropIFace)
-
-function MediaServer2() {
-    this._init.apply(this, arguments);
+function MediaServer2(owner) {
+    return new MediaServer2Proxy(Gio.DBus.session, owner,
+                                 '/org/mpris/MediaPlayer2');
 }
 
-MediaServer2.prototype = {
-    _init: function(owner) {
-        DBus.session.proxifyObject(this, owner, '/org/mpris/MediaPlayer2', this);
-    },
-    getIdentity: function(callback) {
-        this.GetRemote('Identity', Lang.bind(this,
-            function(identity, ex) {
-                if (!ex)
-                    callback(this, identity);
-            }));
-    },
-    getDesktopEntry: function(callback) {
-        this.GetRemote('DesktopEntry', Lang.bind(this,
-            function(entry, ex) {
-                if (!ex)
-                    callback(this, entry);
-            }));
-    },
-    getRaise: function(callback) {
-        this.GetRemote('CanRaise', Lang.bind(this,
-            function(raise, ex) {
-                if (!ex)
-                    callback(this, raise);
-            }));
-    },
-    getQuit: function(callback) {
-        this.GetRemote('CanQuit', Lang.bind(this,
-            function(quit, ex) {
-                if (!ex)
-                    callback(this, quit);
-            }));
-    }
-}
-DBus.proxifyPrototype(MediaServer2.prototype, MediaServer2IFace)
-
-function MediaServer2Player() {
-    this._init.apply(this, arguments);
+function MediaServer2Player(owner) {
+    return new MediaServer2PlayerProxy(Gio.DBus.session, owner,
+                                       '/org/mpris/MediaPlayer2');
 }
 
-MediaServer2Player.prototype = {
-    _init: function(owner) {
-        this._owner = owner;
-        DBus.session.proxifyObject(this, owner, '/org/mpris/MediaPlayer2', this);
-    },
-    getMetadata: function(callback) {
-        this.GetRemote('Metadata', Lang.bind(this,
-            function(metadata, ex) {
-                if (!ex)
-                    callback(this, metadata);
-            }));
-    },
-    getPlaybackStatus: function(callback) {
-        this.GetRemote('PlaybackStatus', Lang.bind(this,
-            function(status, ex) {
-                if (!ex)
-                    callback(this, status);
-            }));
-    },
-    getPosition: function(callback) {
-        this.GetRemote('Position', Lang.bind(this,
-            function(position, ex) {
-                if (!ex)
-                    callback(this, position);
-                else
-                    callback(this, null);
-            }));
-    },
-    getShuffle: function(callback) {
-        this.GetRemote('Shuffle', Lang.bind(this,
-            function(shuffle, ex) {
-                if (!ex)
-                    callback(this, shuffle);
-            }));
-    },
-    setShuffle: function(value) {
-        this.SetRemote('Shuffle', value);
-    },
-    getVolume: function(callback) {
-        this.GetRemote('Volume', Lang.bind(this,
-            function(volume, ex) {
-                if (!ex)
-                    callback(this, volume);
-                else
-                    callback(this, null);
-            }));
-    },
-    setVolume: function(value) {
-        this.SetRemote('Volume', parseFloat(value));
-    },
-    getRepeat: function(callback) {
-        this.GetRemote('LoopStatus', Lang.bind(this,
-            function(repeat, ex) {
-                if (!ex) {
-                    if (repeat == "None")
-                        repeat = false
-                    else
-                        repeat = true
-                    callback(this, repeat);
-                }
-            }));
-    },
-    setRepeat: function(value) {
-        if (value)
-            value = "Playlist"
-        else
-            value = "None"
-        this.SetRemote('LoopStatus', value);
-    }
+function MediaServer2Playlists(owner) {
+    return new MediaServer2PlaylistsProxy(Gio.DBus.session, owner,
+                                          '/org/mpris/MediaPlayer2');
 }
-DBus.proxifyPrototype(MediaServer2Player.prototype, MediaServer2PlayerIFace)
-
-function MediaServer2Playlists() {
-    this._init.apply(this, arguments);
-}
-
-MediaServer2Playlists.prototype = {
-    _init: function(owner) {
-        this._owner = owner;
-        DBus.session.proxifyObject(this, owner, '/org/mpris/MediaPlayer2', this);
-    },
-    getPlaylistCount: function(callback) {
-        this.GetRemote('PlaylistCount', Lang.bind(this,
-            function(count, ex) {
-                if (!ex)
-                    callback(this, count);
-            }));
-    },
-    getOrderings: function(callback) {
-        this.GetRemote('Orderings', Lang.bind(this,
-            function(orderings, ex) {
-                if (!ex)
-                    callback(this, orderings);
-            }));
-    },
-    getActivePlaylist: function(callback) {
-        this.GetRemote('ActivePlaylist', Lang.bind(this,
-            function(active, ex) {
-                if (!ex)
-                    callback(this, active);
-            }));
-    }
-}
-DBus.proxifyPrototype(MediaServer2Playlists.prototype, MediaServer2PlaylistsIFace)
