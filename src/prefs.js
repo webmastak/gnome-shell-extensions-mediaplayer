3e2fe069122811951d407bf6aaf395800788d807
Jean-Philippe Braun <eon@patapon.info>
2012-09-19 00:38:12 +0200

    Add a position setting (center, right, volume-menu)

--- src/prefs.js
+++ src/prefs.js
@@ -15,6 +15,7 @@
 **/
 
 const Gtk = imports.gi.Gtk;
+const GObject = imports.gi.GObject;
 
 const Gettext = imports.gettext.domain('gnome-shell-extensions-mediaplayer');
 const _ = Gettext.gettext;
@@ -22,76 +23,200 @@ const _ = Gettext.gettext;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Lib = Me.imports.lib;
 
+let gsettings;
 let settings;
-let settings_bool;
-let settings_range;
 
 function init() {
     Lib.initTranslations(Me);
-    settings = Lib.getSettings(Me);
-    settings_bool = {
-        volumemenu: {label: _("Show the media player in the volume menu"),
-                     help: _("Restart the shell to apply this setting.")},
-        rundefault: {label: _("Allow to start the default media player"),
-                     help: _("Runs the default mediaplayer by clicking on the media player status icon.")},
-        volume: {label: _("Show the media player volume slider")},
-        position: {label: _("Show the media player position slider")},
-        playlists: {label: _("Show media player playlists")},
-        rating: {label: _("Display song rating"),
-                 help: _("Display the currently playing song's rating on a 0 to 5 scale")}
-    };
-    settings_range = {
-        coversize: {label: _("Album cover size"),
-                    help: _("The size of the cover displayed in the menu. Default is 80px width."),
-                    min: 50, max: 110, step: 5, default: 80}
+    gsettings = Lib.getSettings(Me);
+    settings = {
+        indicator_position: {
+            type: "e",
+            label: _("The position of the indicator in the top panel"),
+            help: _("Restart the shell to apply this setting."),
+            list: [
+                { nick: "center", name: _('Center'), id: 0 },
+                { nick: "right", name: _('Right'), id: 1 },
+                { nick: "volume-menu", name: _('Volume menu'), id: 2 }
+            ]
+        },
+        rundefault: {
+            type: "b",
+            label: _("Allow to start the default media player"),
+            help: _("Runs the default mediaplayer by clicking on the media player status icon.")
+        },
+        volume: {
+            type: "b",
+            label: _("Show the media player volume slider")
+        },
+        position: {
+            type: "b",
+            label: _("Show the media player position slider")
+        },
+        playlists: {
+            type: "b",
+            label: _("Show media player playlists")
+        },
+        rating: {
+            type: "b",
+            label: _("Display song rating"),
+            help: _("Display the currently playing song's rating on a 0 to 5 scale")
+        },
+        coversize: {
+            type: "r",
+            label: _("Album cover size"),
+            help: _("The size of the cover displayed in the menu. Default is 80px width."),
+            min: 50,
+            max: 110,
+            step: 5,
+            default: 80
+        }
     };
 }
 
-function createRangeSetting(setting) {
+function buildPrefsWidget() {
+    let frame = new Gtk.Box({orientation: Gtk.Orientation.VERTICAL,
+                             border_width: 10 });
+    let vbox = new Gtk.Box({orientation: Gtk.Orientation.VERTICAL,
+                            margin: 20, margin_top: 10 });
+    let hbox;
 
-    let hbox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+    for (setting in settings) {
+        if (settings[setting].type == 's')
+            hbox = createStringSetting(setting);
+        if (settings[setting].type == "i")
+            hbox = createIntSetting(setting);
+        if (settings[setting].type == "b")
+            hbox = createBoolSetting(setting);
+        if (settings[setting].type == "r")
+            hbox = createRangeSetting(setting);
+        if (settings[setting].type == "e")
+            hbox = createEnumSetting(setting);
+        vbox.add(hbox);
+    }
 
-    let setting_label = new Gtk.Label({ label: settings_range[setting].label,
-                                        xalign: 0 });
+    frame.add(vbox);
+    frame.show_all();
+    return frame;
+}
 
-    let setting_range = Gtk.HScale.new_with_range(settings_range[setting].min,
-                                                  settings_range[setting].max,
-                                                  settings_range[setting].step);
-    setting_range.set_value(settings.get_int(setting));
-    setting_range.set_draw_value(false);
-    setting_range.add_mark(settings_range[setting].default,
-                           Gtk.PositionType.BOTTOM, null);
-    setting_range.set_size_request(200, -1);
-    setting_range.connect('value-changed', function(slider) {
-        settings.set_int(setting, slider.get_value());
+function createEnumSetting(setting) {
+
+    let hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL,
+                            margin_top: 5});
+
+    let setting_label = new Gtk.Label({label: settings[setting].label,
+                                       xalign: 0 });
+
+    let model = new Gtk.ListStore();
+    model.set_column_types([GObject.TYPE_INT, GObject.TYPE_STRING]);
+    let setting_enum = new Gtk.ComboBox({model: model});
+    setting_enum.get_style_context().add_class(Gtk.STYLE_CLASS_RAISED);
+    let renderer = new Gtk.CellRendererText();
+    setting_enum.pack_start(renderer, true);
+    setting_enum.add_attribute(renderer, 'text', 1);
+
+    for (let i=0; i<settings[setting].list.length; i++) {
+        let item = settings[setting].list[i];
+        let iter = model.append();
+        model.set(iter, [0, 1], [item.id, item.name]);
+        if (item.id == gsettings.get_enum(setting.replace('_', '-'))) {
+            setting_enum.set_active(item.id);
+        }
+    }
+
+    setting_enum.connect('changed', function(entry) {
+        let [success, iter] = setting_enum.get_active_iter();
+        if (!success)
+            return;
+
+        let id = model.get_value(iter, 0)
+        gsettings.set_enum(setting.replace('_', '-'), id);
     });
 
-    if (settings_range[setting].help) {
-        setting_label.set_tooltip_text(settings_range[setting].help)
-        setting_range.set_tooltip_text(settings_range[setting].help)
+    if (settings[setting].help) {
+        setting_label.set_tooltip_text(settings[setting].help)
+        setting_enum.set_tooltip_text(settings[setting].help)
     }
 
     hbox.pack_start(setting_label, true, true, 0);
-    hbox.add(setting_range);
+    hbox.add(setting_enum);
+
+    return hbox;
+
+}
+
+function createStringSetting(setting) {
+
+    let hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL,
+                            margin_top: 5});
+
+    let setting_label = new Gtk.Label({label: settings[setting].label,
+                                       xalign: 0 });
+
+    let setting_string = new Gtk.Entry({text: gsettings.get_string(setting.replace('_', '-'))});
+    setting_string.connect('notify::text', function(entry) {
+        gsettings.set_string(setting.replace('_', '-'), entry.text);
+    });
+
+    if (settings[setting].mode == "passwd") {
+        setting_string.set_visibility(false);
+    }
+
+    if (settings[setting].help) {
+        setting_label.set_tooltip_text(settings[setting].help)
+        setting_string.set_tooltip_text(settings[setting].help)
+    }
+
+    hbox.pack_start(setting_label, true, true, 0);
+    hbox.add(setting_string);
+
+    return hbox;
+}
+
+function createIntSetting(setting) {
+
+    let hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL,
+                            margin_top: 5});
+
+    let setting_label = new Gtk.Label({label: settings[setting].label,
+                                       xalign: 0 });
+
+    let adjustment = new Gtk.Adjustment({ lower: 1, upper: 65535, step_increment: 1});
+    let setting_int = new Gtk.SpinButton({adjustment: adjustment,
+                                          snap_to_ticks: true});
+    setting_int.set_value(gsettings.get_int(setting.replace('_', '-')));
+    setting_int.connect('value-changed', function(entry) {
+        gsettings.set_int(setting.replace('_', '-'), entry.value);
+    });
+
+    if (settings[setting].help) {
+        setting_label.set_tooltip_text(settings[setting].help)
+        setting_int.set_tooltip_text(settings[setting].help)
+    }
+
+    hbox.pack_start(setting_label, true, true, 0);
+    hbox.add(setting_int);
 
     return hbox;
 }
 
 function createBoolSetting(setting) {
 
-    let hbox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+    let hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL,
+                            margin_top: 5});
 
-    let setting_label = new Gtk.Label({label: settings_bool[setting].label,
+    let setting_label = new Gtk.Label({label: settings[setting].label,
                                        xalign: 0 });
 
-    let setting_switch = new Gtk.Switch({active: settings.get_boolean(setting)});
+    let setting_switch = new Gtk.Switch({active: gsettings.get_boolean(setting.replace('_', '-'))});
     setting_switch.connect('notify::active', function(button) {
-        settings.set_boolean(setting, button.active);
+        gsettings.set_boolean(setting.replace('_', '-'), button.active);
     });
 
-    if (settings_bool[setting].help) {
-        setting_label.set_tooltip_text(settings_bool[setting].help)
-        setting_switch.set_tooltip_text(settings_bool[setting].help)
+    if (settings[setting].help) {
+        setting_label.set_tooltip_text(settings[setting].help)
+        setting_switch.set_tooltip_text(settings[setting].help)
     }
 
     hbox.pack_start(setting_label, true, true, 0);
@@ -100,23 +225,32 @@ function createBoolSetting(setting) {
     return hbox;
 }
 
-function buildPrefsWidget() {
-    let frame = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL,
-                              border_width: 10 });
-    let vbox = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL,
-                             margin: 20, margin_top: 10 });
+function createRangeSetting(setting) {
 
-    for (setting in settings_bool) {
-        let hbox = createBoolSetting(setting);
-        vbox.add(hbox);
+    let hbox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL });
+
+    let setting_label = new Gtk.Label({ label: settings[setting].label,
+                                        xalign: 0 });
+
+    let setting_range = Gtk.HScale.new_with_range(settings[setting].min,
+                                                  settings[setting].max,
+                                                  settings[setting].step);
+    setting_range.set_value(gsettings.get_int(setting));
+    setting_range.set_draw_value(false);
+    setting_range.add_mark(settings[setting].default,
+                           Gtk.PositionType.BOTTOM, null);
+    setting_range.set_size_request(200, -1);
+    setting_range.connect('value-changed', function(slider) {
+        gsettings.set_int(setting, slider.get_value());
+    });
+
+    if (settings[setting].help) {
+        setting_label.set_tooltip_text(settings[setting].help)
+        setting_range.set_tooltip_text(settings[setting].help)
     }
 
-    for (setting in settings_range) {
-        let hbox = createRangeSetting(setting);
-        vbox.add(hbox);
-    }
+    hbox.pack_start(setting_label, true, true, 0);
+    hbox.add(setting_range);
 
-    frame.add(vbox);
-    frame.show_all();
-    return frame;
+    return hbox;
 }
