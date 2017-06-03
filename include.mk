092410192ef92baecf365b11c532b13368d14b6a
Jason Gray <jasonlevigray3@gmail.com>
2017-06-03 18:07:47 -0500

    Update include.mk

--- include.mk
+++ include.mk
@@ -4,4 +4,4 @@ extensionname = mediaplayer
 uuid = $(extensionname)@patapon.info
 extensiondir = $(topextensiondir)/$(uuid)
 localextensiondir = $(HOME)/.local/share/gnome-shell/extensions/$(uuid)
-extensionurl = http://github.com/eonpatapon/gnome-shell-extensions-mediaplayer
+extensionurl = https://github.com/JasonLG1979/gnome-shell-extensions-mediaplayer
