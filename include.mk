80a615be658845a6b85b590688eee589b927fac3
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2017-07-18 17:14:42 +0200

    port to meson

--- include.mk
+++
@@ -1,7 +0,0 @@
-gschemabase = org.gnome.shell.extensions
-topextensiondir = $(datadir)/gnome-shell/extensions
-extensionname = mediaplayer
-uuid = $(extensionname)@patapon.info
-extensiondir = $(topextensiondir)/$(uuid)
-localextensiondir = $(HOME)/.local/share/gnome-shell/extensions/$(uuid)
-extensionurl = https://github.com/JasonLG1979/gnome-shell-extensions-mediaplayer
